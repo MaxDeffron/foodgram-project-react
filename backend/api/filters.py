79b261f9c83ed7a414ac2c6464943bd945005383
Max <deffron@yandex.ru>
2022-04-26 21:17:17 +0500

    flake

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters import AllValuesMultipleFilter, rest_framework as filters
+from django_filters import AllValuesMultipleFilter, rest_framework
 from django_filters.widgets import BooleanWidget
 from rest_framework.filters import SearchFilter
 
@@ -9,9 +9,9 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilter(filters.FilterSet):
-    is_in_shopping_cart = filters.BooleanFilter(widget=BooleanWidget())
-    is_favorited = filters.BooleanFilter(widget=BooleanWidget())
+class RecipeFilter(rest_framework.FilterSet):
+    is_in_shopping_cart = rest_framework.BooleanFilter(widget=BooleanWidget())
+    is_favorited = rest_framework.BooleanFilter(widget=BooleanWidget())
     tags = AllValuesMultipleFilter(field_name='tags__slug')
     author = AllValuesMultipleFilter(field_name='author__id')
 
