1159438376dcc521827b9dc79559539ec6a2a625
Max <deffron@yandex.ru>
2022-04-27 15:11:41 +0500

    fix flake4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.db.models import F
-from djoser.serializers import (UserCreateSerializer, UserSerializer)
-from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
 from users.models import Follow
 
 User = get_user_model()
