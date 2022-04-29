c9c132b6fa77f5d788f701619dca1572e8734f37
Max <deffron@yandex.ru>
2022-04-29 17:47:48 +0500

    ser fix4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from django.db.models import F
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
