05b5d5697dc134d44d186df568550ae2238a94a3
Max <deffron@yandex.ru>
2022-05-03 23:45:38 +0500

    ingr ser6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -89,7 +89,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('tags', 'ingredients', 'name',
-                  'image', 'text', 'cooking_time', 'amount')
+                  'image', 'text', 'cooking_time')
 
     def get_ingredients(self, obj):
         return obj.ingredients.values(
@@ -100,15 +100,15 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         ingredients = data.get('ingredients', None)
         ingredients_set = set()
         for ingredient in ingredients:
-            if type(data['amount']) is str:
-                if not ingredient.get('amount').isdigit():
-                    raise serializers.ValidationError(
-                        ('Количество ингредиента должно быть числом')
-                    )
-            if int(data['amount']) <= 0:
-                raise serializers.ValidationError(
-                    ('Минимальное количество ингридиентов 1')
-                )
+            # if type(data['ingredients.amount']) is str:
+            #     if not ingredient.get('ingredients.amount').isdigit():
+            #         raise serializers.ValidationError(
+            #             ('Количество ингредиента должно быть числом')
+            #         )
+            # if int(data['amount']) <= 0:
+            #     raise serializers.ValidationError(
+            #         ('Минимальное количество ингридиентов 1')
+            #     )
             if int(data['cooking_time']) <= 0:
                 raise serializers.ValidationError(
                     'Время готовки должно быть > 0 '
